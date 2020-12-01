2c97f64b184546e6be15e3289f2836ea45d3ac6b
haaid <me@wanghaida.com>
2020-12-01 11:40:26 +0800

    feat: 测试界面

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    'extends': [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+    ],
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    rules: {
+        // 强制 error
+        indent: ['error', 4, { SwitchCase: 1 }],
+        'vue/html-indent': ['error', 4],
+        'vue/html-quotes': ['error', 'double'],
+        'no-empty': ['error', { allowEmptyCatch: true }],
+        // 推荐 warn
+        'max-len': ['warn', { code: 200 }],
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        // 关闭 off
+        'no-param-reassign': 'off',
+    },
+};
---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    runtimeCompiler: true,
+    css: {
+        extract: false,
+    },
+    // 修改 主入口 为 examples 目录
+    pages: {
+        index: {
+            entry: 'examples/main.js',
+        },
+    },
+    // 扩展 webbpack 配置，使 import 可以解构
+    configureWebpack: {
+        output: {
+            libraryExport: 'default',
+        },
+    },
+};
